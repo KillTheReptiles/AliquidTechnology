21ef16acbb859ed5459ffdebb3fc217a8be347f5
KillTheReptiles <duvan.diaz.2019@upb.edu.co>
2020-03-29 15:08:36 -0500

    Login y Registro

---
+++ Pagina web/AliquidTecnology/src/main/java/Controlador/userController.java
@@ -0,0 +1,85 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controlador;
+
+import Modelo.Usuario;
+import javax.inject.Named;
+import javax.enterprise.context.SessionScoped;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+import javax.json.JsonObject;
+
+/**
+ *
+ * @author VAL
+ */
+@Named(value = "userController")
+@SessionScoped
+public class userController implements Serializable {
+   Usuario usuario;
+   private String userName;
+    private String correo;
+    private String clave;
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getClave() {
+        return clave;
+    }
+
+    public void setClave(String clave) {
+        this.clave = clave;
+    }
+
+    public List<Usuario> getListausuario() {
+        return listausuario;
+    }
+
+    public void setListausuario(List<Usuario> listausuario) {
+        this.listausuario = listausuario;
+    }
+    
+    private List<Usuario> listausuario;
+    /**
+     * Creates a new instance of userController
+     */
+    public userController() {
+        usuario= new Usuario();
+        listausuario= new ArrayList<Usuario>();
+    }
+   
+    public void registrar(){
+        FacesContext context = FacesContext.getCurrentInstance();
+        ExternalContext ex = context.getExternalContext();
+        //Redirigir a inicio y salir mensaje bonitoxd
+        
+    }
+}
