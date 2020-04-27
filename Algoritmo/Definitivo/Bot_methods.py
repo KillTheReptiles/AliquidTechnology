89776ce6e8e25edf6a741f53682fcf5b15c00ef2
Duvan Diaz <47454957+KillTheReptiles@users.noreply.github.com>
2020-04-27 00:10:20 -0500

    ultimo

--- Algoritmo/Definitivo/Bot_methods.py
+++ Algoritmo/Definitivo/Bot_methods.py
@@ -2,7 +2,7 @@ import pickle
 import nltk
 from nltk.stem import WordNetLemmatizer
 import numpy as np
-#from keras.models import load_model
+# from keras.models import load_model
 from tensorflow.keras.models import load_model
 from baseDatos import db
 
@@ -21,18 +21,29 @@ class ElBot:
             self.intents.append(contPregunta)
 
         self.categoriaFinal = ""
+        self.escogida=False
+
 
         self.especificaciones = {
-            1: "Precio",
-            2: "Ram"
-        }
 
+            1: "Company",
+            2: "Product",
+            3: "Inches",
+            4: "ScreenResolution",
+            5: "Cpu",
+            6: "Ram",
+            7: "Memory",
+            8: "Gpu",
+            9: "OpSys",
+            10: "Weight",
+            11: "Price_euros"
+
+        }
 
     def clean_up_sentence(self, sentence):
         sentence_words = nltk.word_tokenize(sentence)
         sentence_words = [self.lemmatizer.lemmatize(word.lower()) for word in sentence_words]
         return sentence_words
-
 
     # return bag of words array: 0 or 1 for each word in the bag that exists in the sentence
     def bow(self, sentence, words, show_details=True):
@@ -49,7 +60,6 @@ class ElBot:
                         print("found in bag: %s" % w)
         return (np.array(bag))
 
-
     def predict_class(self, sentence, model):
         # filter out predictions below a threshold
         p = self.bow(sentence, self.words, show_details=False)
@@ -63,7 +73,6 @@ class ElBot:
             return_list.append({"intent": self.classes[r[0]], "probability": str(r[1])})
         return return_list
 
-
     def getResponse(self, ints, intents):
         tag = ints[0]['intent']
         list_of_intents = intents
@@ -74,53 +83,47 @@ class ElBot:
                 break
         return result
 
-
     def chatbot_response(self, msg):
         ints = self.predict_class(msg, self.model)
         tag = ints[0]['intent']
-        '''
-        if(tag == "saludos" or tag=="despedidas" or tag == "preguntasAmables" or tag=="sinRespuesta"):
-            res = getResponse(ints, intents) + "\n"  
-        else:
-            res = 
-        '''
+
         res = self.getResponse(ints, self.intents) + "\n"
         print("<TAG> " + tag)
 
-        if(self.categoriaFinal != ''):
+        if (len(msg) == 1 and self.categoriaFinal!=''):
+            msg= int(msg)
             tipo_compu = db[self.categoriaFinal]
+            if(msg==1 or msg==2 or msg==3 or msg==4 or msg==5 or msg==6 or msg==7 or msg==8 or msg==9 or msg==10 or msg==11):
+                esp=self.especificaciones[msg]
+                print("")
 
-            for conteCompu in tipo_compu.find():
-                # db.inventory.find( { }, { "Product": 1, _id: 0 } )
 
-                for especificacion in conteCompu['Product']:
-                    res = res + str(especificacion)
+                for conteCompu in tipo_compu.find():
+                    # db.inventory.find( { }, { "Product": 1, _id: 0 } )
 
-                res += "\n"
+                    for especificacion in conteCompu[esp]:
+                        res = res + str(especificacion)
+
+                    res += "\n"
+            else:
+                res = "No haz marcado ninguna de las opciones :(!, pregunta de nuevo :D"
 
         return res
 
 
 if __name__ == '__main__':
     bot = ElBot()
-    res = bot.chatbot_response("hola")
-    print(res)
+    while True:
+        msg = input()
+        if(len(msg)>1 ):
 
-'''
-print("INICIO DE LA CONVERSACION\n")
-while True:
-    print("Tu: ")
-    msg = input()
 
-    ints = predict_class(msg, model)
-    tag = ints[0]['intent']
+            ints = bot.predict_class(msg, bot.model)
+            tag = ints[0]['intent']
 
-    if (tag != "saludos" and tag != "despedidas" and tag != "preguntasAmables" and tag != "sinRespuesta"):
-        categoriaFinal = tag
+            if (tag != "saludos" and tag != "despedidas" and tag != "preguntasAmables" and tag != "sinRespuesta"):
+                bot.categoriaFinal = tag
 
-    if msg != '':
-        res = chatbot_response(msg)
-        print("Ali: " + res)
-    else:
-        print("di algo porfa")
-'''
+        res = bot.chatbot_response(msg)
+
+        print(res)
