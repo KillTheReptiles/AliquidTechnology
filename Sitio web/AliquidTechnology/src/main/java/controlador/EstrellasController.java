e0d46dcd0587a63833b98b9e44326c4aadd8c91b
Duvan Diaz <47454957+KillTheReptiles@users.noreply.github.com>
2020-05-06 21:52:40 -0500

    Estadisticas

--- Sitio web/AliquidTechnology/src/main/java/controlador/EstrellasController.java
+++ Sitio web/AliquidTechnology/src/main/java/controlador/EstrellasController.java
@@ -8,9 +8,14 @@ package controlador;
 import javax.inject.Named;
 import javax.enterprise.context.SessionScoped;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import javax.ejb.EJB;
+import javax.faces.model.DataModel;
+import javax.faces.model.ListDataModel;
 import javax.inject.Inject;
 import modelo.Estrellas;
+import modelo.Usuario;
 import upb.edu.co.dao.EstrellasDAO;
 
 /**
@@ -20,37 +25,52 @@ import upb.edu.co.dao.EstrellasDAO;
 @Named(value = "estrellasController")
 @SessionScoped
 public class EstrellasController implements Serializable {
-    
+
+  
     private int cont;
     private int suma;
     private int calificacionActual;
     private Estrellas current;
-    
+    private DataModel<Estrellas> items = null;
+    private List<Double> prom;
+
     @EJB
     EstrellasDAO ejbFacade;
-    
+
     @Inject
     private UserController auth;
- 
+
     public EstrellasController() {
-         current  = new Estrellas();
+        current = new Estrellas();
+       
     }
-    
+
+    public DataModel<Estrellas> getItems() {
+        items = new ListDataModel<Estrellas>(ejbFacade.findAll());
+
+        return items;
+    }
+ 
+
+    public String prepareList() {
+        items = new ListDataModel<Estrellas>(ejbFacade.findAll());
+        return "estadisticas?faces-redirect=true";
+    }
+
     public void ActualizarCal() {
-        
+
         Estrellas actual = (Estrellas) ejbFacade.find(auth.getUsuarioAutenticado().getCorreo());
         cont = actual.getContador();
         suma = actual.getSumaCalificaciones();
-        
-        current.setContador( cont+1);   
-        current.setSumaCalificaciones(calificacionActual +suma);
+
+        current.setContador(cont + 1);
+        current.setSumaCalificaciones(calificacionActual + suma);
         current.setCorreoUsuario(auth.getUsuarioAutenticado().getCorreo());
-        
+
         ejbFacade.edit(current);
-        current  = new Estrellas();
-    
+        current = new Estrellas();
+
     }
-    
 
     public int getCalificacionActual() {
         return calificacionActual;
@@ -75,5 +95,26 @@ public class EstrellasController implements Serializable {
     public void setAuth(UserController auth) {
         this.auth = auth;
     }
-    
+
+  
+    public List<Double> getProm() {
+        double promedio;
+         prom=new ArrayList<Double>();
+        for (Estrellas item : items) {
+
+            if (item.getContador() == 0) {
+                promedio = 0;
+
+            } else {
+                promedio = (double) item.getSumaCalificaciones() / (double) item.getContador();
+            }
+            prom.add(promedio);
+        }
+        return prom;
+    }
+
+    public void setProm(List<Double> prom) {
+        this.prom = prom;
+    }
+
 }
